524e10f25161e41cda732e8d5f7aaaba44f51f2f
toufiq178 <tajahmedtoufiq1@gmail.com>
2026-02-25 20:18:01 +0600

    first commit

---
+++ varLetConst.js
@@ -0,0 +1,33 @@
+
+var name = 'rofik';
+var name = 'sofik';
+name = 'jobbar'
+// console.log(name);
+
+
+let country = 'bangladesh';
+// let country = 'india';
+country = 'singapore';
+// console.log(country);
+
+
+const age  = 12 ;
+// const age = 30;
+// age = 20 ;
+// console.log(age);
+
+
+const student = {
+
+    name : 'ali',
+    roll : 9 ,
+    pass : true
+}
+
+student.name = 'abu'
+console.log(student);
+
+const array = [ 1,3,4,5];
+
+array.unshift(10);
+console.log(array);
