1b7f1a8c98c6dbd1d92a7d16d914de0f8a98f2df
toufiq178 <tajahmedtoufiq1@gmail.com>
2026-02-25 21:22:41 +0600

    module26-2

---
+++ defult-parameters.js
@@ -0,0 +1,28 @@
+function add(number , number2 = 0) {
+    
+    const total = number + number2 ;
+    console.log(total);
+    
+}
+
+// add(10);
+
+
+function multiply(num1 , num2 = 1) {
+    
+    const multiply = num1 * num2 ;
+    console.log(multiply);
+    
+}
+
+multiply(10 )
+
+
+function fullName( first , last = '') {
+    
+    const fullName = first + ' ' + last ;
+    console.log(fullName);
+    
+}
+
+fullName('atik')
